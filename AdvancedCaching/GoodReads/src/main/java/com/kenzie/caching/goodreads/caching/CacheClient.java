2acc1ef7e077ddb0c2ad403e51c44423ed34041f
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2023-11-21 01:41:04 -0500

    piecing together CacheClient in goodReads

--- AdvancedCaching/GoodReads/src/main/java/com/kenzie/caching/goodreads/caching/CacheClient.java
+++ AdvancedCaching/GoodReads/src/main/java/com/kenzie/caching/goodreads/caching/CacheClient.java
@@ -1,12 +1,29 @@
 package com.kenzie.caching.goodreads.caching;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.inject.Inject;
 
 public class CacheClient {
+    private final JedisPool pool;
+
+
     @Inject
     public CacheClient(JedisPool jedisPool) {
+        this.pool = jedisPool;
+    }
+
+
+        public void setValue(String key, int seconds, String value) {
+            if (key == null) {
+                throw new IllegalArgumentException("Key is null");
+            }
+
+            try (Jedis jedis = pool.getResource()) {
+                jedis.setex(key,seconds,value);
+
+            }
 
     }
 }
