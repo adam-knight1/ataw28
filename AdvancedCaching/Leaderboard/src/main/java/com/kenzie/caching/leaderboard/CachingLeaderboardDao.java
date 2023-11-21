95cb5573dc6f099cdfe54377595f3ae64dba9ee0
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2023-11-20 23:21:42 -0500

    piecing together CachingLeaderboardDao

--- AdvancedCaching/Leaderboard/src/main/java/com/kenzie/caching/leaderboard/CachingLeaderboardDao.java
+++ AdvancedCaching/Leaderboard/src/main/java/com/kenzie/caching/leaderboard/CachingLeaderboardDao.java
@@ -1,12 +1,15 @@
 package com.kenzie.caching.leaderboard;
 
+import com.kenzie.caching.leaderboard.resources.datasource.Entry;
 import com.kenzie.caching.leaderboard.resources.datasource.LeaderboardDao;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 public class CachingLeaderboardDao {
     private final LeaderboardDao dataSource;
     private final CacheClient cache;
+    private static final int TTL = 300;
 
     /**
      * Constructor.
@@ -23,13 +26,29 @@ public class CachingLeaderboardDao {
     /**
      * Retrieves score associated with the specified user. Should use the cache when possible, but the dataSource object
      * is our source of truth for high scores. The TTL for our high scores should be 5 minutes.
-     *
+     * <p>
      * PARTICIPANTS: replace return 0 with your implementation of this method.
      *
      * @param username String representing player username
      * @return long representing score associated with username
      */
     public long getHighScore(String username) {
-        return 0;
+        Optional<String> cachedScore = cache.getValue(username);
+        if (cachedScore.isPresent()) {
+            return Long.parseLong(cachedScore.get()); //string??
+        } else {
+            Entry entry = dataSource.getEntry(username);
+            if (entry != null) {
+                long score = entry.getScore();
+                cache.setValue(username, TTL, String.valueOf(score));
+                return score;
+
+            } else {
+                System.out.println("user not found");
+                return 0;
+
+
+            }
+        }
     }
 }
